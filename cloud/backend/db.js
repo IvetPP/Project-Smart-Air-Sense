3b1fbcd34f189bdf045867eaf57dab4a86855143
Kika Kita <kika.kita@plus4u.net>
2026-01-06 15:49:20 +0100

    fix: supabase connection fixes

---
+++ cloud/backend/db.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const { createClient } = require('@supabase/supabase-js');
+
+// These should be set in Render's Environment Variables
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+module.exports = supabase;
