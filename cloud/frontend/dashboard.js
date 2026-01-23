5b1ac42b7bf176391617593221edcd07b612427e
Kika Kita <155003883+ki-kit@users.noreply.github.com>
2026-01-23 09:25:57 +0100

    Update dashboard.js - capital t to lowercase t in true

--- cloud/frontend/dashboard.js
+++ cloud/frontend/dashboard.js
@@ -119,7 +119,7 @@ $(document).ready(function () {
                     if (p <1013) stateText  ="Lower";
                     else if (p> 1013) stateText = "Higher"
 
-                    updateBox('bar', p, True, stateText)
+                    updateBox('bar', p, true, stateText)
 
                     /*
                     // old code for when pressure had a warning
